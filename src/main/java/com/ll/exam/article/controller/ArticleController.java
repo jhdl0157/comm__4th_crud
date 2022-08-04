4458e0db0c4268f96630c490ce59817ddb954088
jhdl0157 <jhdl0157@naver.com>
2022-08-04 22:51:09 +0900

    DB & 토스트 ui 적용

---
+++ src/main/java/com/ll/exam/article/controller/ArticleController.java
@@ -0,0 +1,72 @@
+package com.ll.exam.article.controller;
+
+import com.ll.exam.Rq;
+import com.ll.exam.annotation.Autowired;
+import com.ll.exam.annotation.Controller;
+import com.ll.exam.annotation.GetMapping;
+import com.ll.exam.annotation.PostMapping;
+import com.ll.exam.article.dto.ArticleDto;
+import com.ll.exam.article.service.ArticleService;
+
+import java.util.List;
+
+// ArticleController 가 컨트롤러 이다.
+// 아래 ArticleController 클래스는 Controller 이다.
+@Controller
+public class ArticleController {
+    @Autowired
+    private ArticleService articleService;
+
+    @GetMapping("/usr/article/list")
+    public void showList(Rq rq) {
+        List<ArticleDto> articleDtos = articleService.getArticles();
+
+        rq.setAttr("articles", articleDtos);
+        rq.view("usr/article/list");
+    }
+
+    @GetMapping("/usr/article/write")
+    public void showWrite(Rq rq) {
+        rq.view("usr/article/write");
+    }
+
+    @PostMapping("/usr/article/write")
+    public void write(Rq rq) {
+        String title = rq.getParam("title", "");
+        String body = rq.getParam("body", "");
+
+        if (title.length() == 0) {
+            rq.historyBack("제목을 입력해주세요.");
+            return;
+        }
+
+        if (body.length() == 0) {
+            rq.historyBack("내용을 입력해주세요.");
+            return;
+        }
+
+        long id = articleService.write(title, body);
+
+        rq.replace("/usr/article/detail/%d".formatted(id), "%d번 게시물이 생성 되었습니다.".formatted(id));
+    }
+
+    @GetMapping("/usr/article/detail/{id}")
+    public void showDetail(Rq rq) {
+        long id = rq.getLongParam("id", 0);
+
+        if (id == 0) {
+            rq.historyBack("번호를 입력해주세요.");
+            return;
+        }
+
+        ArticleDto articleDto = articleService.getArticleById(id);
+
+        if (articleDto == null) {
+            rq.historyBack("해당 글이 존재하지 않습니다.");
+            return;
+        }
+
+        rq.setAttr("article", articleDto);
+        rq.view("usr/article/detail");
+    }
+}
---
+++ src/webapp/jsp/usr/article/list.jsp
@@ -0,0 +1,27 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+
+<%@ include file="../common/head.jspf"%>
+
+<meta charset="UTF-8">
+
+<section>
+    <div class="container px-3 mx-auto">
+        <h1 class="font-bold text-lg">
+            <i class="fa-solid fa-rectangle-list"></i> 목록
+        </h1>
+
+        <ul class="mt-5">
+            <c:forEach items="${articles}" var="article">
+            <li>
+                <a href="/usr/article/detail/${article.id}">
+                    <div class="badge badge-primary">${article.id}</div>
+                    ${article.title}
+                </a>
+            </li>
+            </c:forEach>
+        </ul>
+    </div>
+</section>
+
+<%@ include file="../common/foot.jspf"%>
